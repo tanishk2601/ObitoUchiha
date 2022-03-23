ff8b3c1878ef4152459d4dd360f3744cbac8ff22
KarlexD <98577270+KarlexD@users.noreply.github.com>
2022-03-24 04:06:14 +0530

    Update reporting_sql.py

--- SuzuneBot/modules/sql/reporting_sql.py
+++ SuzuneBot/modules/sql/reporting_sql.py
@@ -1,7 +1,7 @@
 import threading
 from typing import Union
 
-from SuzuneBot.modules.sql import BASE, SESSION
+from VanitasBot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String
 
 
