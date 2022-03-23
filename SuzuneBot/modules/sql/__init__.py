678fb34696830c10bf466867fc57dcfc4a375b9e
KarlexD <98577270+KarlexD@users.noreply.github.com>
2022-03-24 04:23:40 +0530

    Update __init__.py

--- SuzuneBot/modules/sql/__init__.py
+++ SuzuneBot/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from SuzuneBot import DB_URI
+from VanitasBot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
