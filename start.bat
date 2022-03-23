d484fe4406ae3477a6848879e29d398e13c6895b
KarlexD <98577270+KarlexD@users.noreply.github.com>
2022-03-24 03:42:39 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE SuzuneBot
-:: Enables virtual env mode and then starts SuzuneBot
-env\scripts\activate.bat && py -m SuzuneBot
+TITLE VanitasBot
+:: Enables virtual env mode and then starts VanitasBot
+env\scripts\activate.bat && py -m VanitasBot
