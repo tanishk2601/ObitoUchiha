827eb51f77a733c21aa60c464f47524c94635a8e
KarlexD <98577270+KarlexD@users.noreply.github.com>
2022-03-24 03:43:44 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop SuzuneBot
-net start SuzuneBot
+net stop VanitasBot
+net start VanitasBot
