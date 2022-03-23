c7f8ba02822ce5390459f025e8d6dc43723722e9
KarlexD <98577270+KarlexD@users.noreply.github.com>
2022-03-24 03:51:34 +0530

    Update extraction.py

--- SuzuneBot/modules/helper_funcs/extraction.py
+++ SuzuneBot/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from SuzuneBot import LOGGER
-from SuzuneBot.modules.users import get_user_id
+from VanitasBot import LOGGER
+from VanitasBot.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
