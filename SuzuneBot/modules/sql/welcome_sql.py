9b1ec755d5170fb2cf4983659ddabcddad095941
KarlexD <98577270+KarlexD@users.noreply.github.com>
2022-03-24 04:00:30 +0530

    Update welcome_sql.py

--- SuzuneBot/modules/sql/welcome_sql.py
+++ SuzuneBot/modules/sql/welcome_sql.py
@@ -2,8 +2,8 @@ import random
 import threading
 from typing import Union
 
-from SuzuneBot.modules.helper_funcs.msg_types import Types
-from SuzuneBot.modules.sql import BASE, SESSION
+from VanitasBot.modules.helper_funcs.msg_types import Types
+from VanitasBot.modules.sql import BASE, SESSION
 from sqlalchemy import BigInteger, Boolean, Column, Integer, String, UnicodeText
 
 DEFAULT_WELCOME = "Hey {first}, how are you?"
