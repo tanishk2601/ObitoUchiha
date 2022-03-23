e0de8addc43925d3f3d0ed623a362da91a05b9dc
KarlexD <98577270+KarlexD@users.noreply.github.com>
2022-03-24 04:17:51 +0530

    Update connection_sql.py

--- SuzuneBot/modules/sql/connection_sql.py
+++ SuzuneBot/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 
-from SuzuneBot.modules.sql import SESSION, BASE
+from VanitasBot.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
