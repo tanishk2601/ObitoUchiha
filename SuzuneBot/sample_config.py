d3afeff008a8bf9110294cb57c1fc5d7e90cf2fe
KarlexD <98577270+KarlexD@users.noreply.github.com>
2022-03-24 02:57:19 +0530

    Update sample_config.py

--- SuzuneBot/sample_config.py
+++ SuzuneBot/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/SuzuneBot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/VanitasBot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
