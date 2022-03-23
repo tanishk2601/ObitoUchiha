c5bbce90443a9887b329c18b9f452436b5efd8fa
KarlexD <98577270+KarlexD@users.noreply.github.com>
2022-03-24 04:25:40 +0530

    Update ForceSubscribe.py

--- SuzuneBot/modules/ForceSubscribe.py
+++ SuzuneBot/modules/ForceSubscribe.py
@@ -10,9 +10,9 @@ from pyrogram.errors.exceptions.bad_request_400 import (
 )
 from pyrogram.types import ChatPermissions, InlineKeyboardButton, InlineKeyboardMarkup
 
-from SuzuneBot import DRAGONS as SUDO_USERS
-from SuzuneBot import pgram
-from SuzuneBot.modules.sql import forceSubscribe_sql as sql
+from VanitasBot import DRAGONS as SUDO_USERS
+from VanitasBot import pgram
+from VanitasBot.modules.sql import forceSubscribe_sql as sql
 
 logging.basicConfig(level=logging.INFO)
 
