5a2ba7217b3cc9a3513e2ca5a82db34dc3284ce2
KarlexD <98577270+KarlexD@users.noreply.github.com>
2022-03-24 04:08:17 +0530

    Update notes_sql.py

--- SuzuneBot/modules/sql/notes_sql.py
+++ SuzuneBot/modules/sql/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from SuzuneBot.modules.helper_funcs.msg_types import Types
-from SuzuneBot.modules.sql import BASE, SESSION
+from VanitasBot.modules.helper_funcs.msg_types import Types
+from VanitasBot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText, distinct, func
 
 
