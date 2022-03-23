16c93a0b0b955f3e083b5a6e78b61f0d2928259a
KarlexD <98577270+KarlexD@users.noreply.github.com>
2022-03-24 04:11:32 +0530

    Update global_bans_sql.py

--- SuzuneBot/modules/sql/global_bans_sql.py
+++ SuzuneBot/modules/sql/global_bans_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from SuzuneBot.modules.sql import BASE, SESSION
+from VanitasBot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 
 
