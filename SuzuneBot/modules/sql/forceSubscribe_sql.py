5f3329e482bfcaa350e3f6682935478cf6b17be7
KarlexD <98577270+KarlexD@users.noreply.github.com>
2022-03-24 04:12:22 +0530

    Update forceSubscribe_sql.py

--- SuzuneBot/modules/sql/forceSubscribe_sql.py
+++ SuzuneBot/modules/sql/forceSubscribe_sql.py
@@ -16,7 +16,7 @@
 
 from sqlalchemy import Column, Numeric, String
 
-from SuzuneBot.modules.sql import BASE, SESSION
+from VanitasBot.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
