1e4b664e1501f35e3e589abffc5771cbf6c3d9ba
KarlexD <98577270+KarlexD@users.noreply.github.com>
2022-03-24 03:58:24 +0530

    Update night_mode_sql.py

--- SuzuneBot/modules/sql_extended/night_mode_sql.py
+++ SuzuneBot/modules/sql_extended/night_mode_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
-from SuzuneBot.modules.sql import BASE, SESSION
+from VanitasBot.modules.sql import BASE, SESSION
 
 
 class Nightmode(BASE):
