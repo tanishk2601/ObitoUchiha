1e46a8fe4ec52c2d61a97daf11f0e91d476e6722
KarlexD <98577270+KarlexD@users.noreply.github.com>
2022-03-24 04:21:54 +0530

    Update approve_sql.py

--- SuzuneBot/modules/sql/approve_sql.py
+++ SuzuneBot/modules/sql/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Integer, func, distinct
 
-from SuzuneBot.modules.sql import BASE, SESSION
+from VanitasBot.modules.sql import BASE, SESSION
 
 
 class Approvals(BASE):
