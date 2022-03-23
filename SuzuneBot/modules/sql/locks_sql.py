17da0f0e34995150c84d94e93259f84415d2ce52
KarlexD <98577270+KarlexD@users.noreply.github.com>
2022-03-24 04:09:56 +0530

    Update locks_sql.py

--- SuzuneBot/modules/sql/locks_sql.py
+++ SuzuneBot/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from SuzuneBot.modules.sql import SESSION, BASE
+from VanitasBot.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
