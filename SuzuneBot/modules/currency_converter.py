c6259f35b1c1b84c5efe851546fdeb25e21f6dcb
KarlexD <98577270+KarlexD@users.noreply.github.com>
2022-03-27 16:33:58 +0530

    Update currency_converter.py

--- SuzuneBot/modules/currency_converter.py
+++ SuzuneBot/modules/currency_converter.py
@@ -1,5 +1,5 @@
 import requests
-from SuzuneBot import CASH_API_KEY, dispatcher
+from VanitasBot import CASH_API_KEY, dispatcher
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
